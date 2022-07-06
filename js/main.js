df85f4850f6fdac84800b4a339dc4b946d8887d0
ax-ross <ax-ross@yandex.ru>
2022-07-06 10:42:09 +0300

    completed

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next-unique",
+        prevEl: ".swiper-button-prev-unique",
+    },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next-plumbing",
+        prevEl: ".swiper-button-prev-plumbing",
+    },
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 2,
+    spaceBetween: 600,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next-ventilation",
+        prevEl: ".swiper-button-prev-ventilation",
+    },
+});
+
+var swiper4 = new Swiper(".mySwiper4", {
+    slidesPerView: 2,
+    spaceBetween: 600,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next-sewerage",
+        prevEl: ".swiper-button-prev-sewerage",
+    },
+});
+
+
+var swiper5 = new Swiper(".mySwiper5", {
+    slidesPerView: 1,
+    spaceBetween: 100,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next-brands",
+        prevEl: ".swiper-button-prev-brands",
+    },
+});
